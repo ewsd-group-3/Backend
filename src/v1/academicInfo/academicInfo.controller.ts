1a6bbd4028b066dc46c8b4b6f245b8b28e6069f2
PhoneZaw <59553350+PhoneZaw@users.noreply.github.com>
2024-03-14 00:32:14 +0700

    addStatusToAcademicInfo

--- src/v1/academicInfo/academicInfo.controller.ts
+++ src/v1/academicInfo/academicInfo.controller.ts
@@ -9,6 +9,7 @@ import catchAsync from '../../utils/catchAsync';
 import successResponse from '../../utils/successResponse';
 import pick from '../../utils/pick';
 import { Semester } from '@prisma/client';
+import { stat } from 'fs';
 
 const createAcademicInfo = catchAsync(async (req, res) => {
   const { name, startDate, endDate, semesters } = req.body;
@@ -36,17 +37,39 @@ const createAcademicInfo = catchAsync(async (req, res) => {
 const getAcademicInfos = catchAsync(async (req, res) => {
   const filter = pick(req.query, ['name']);
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
-  const result = await academicInfoService.queryAcademicInfos(filter, options);
+  const { page, limit, count, totalPages, academicInfos } =
+    await academicInfoService.queryAcademicInfos(filter, options);
   successResponse(res, httpStatus.OK, AppMessage.retrievedSuccessful, {
-    ...result
+    page,
+    limit,
+    count,
+    totalPages,
+    academicInfos: academicInfos.map((academicInfo) => ({
+      ...academicInfo,
+      status: getAcademicStatus(academicInfo.startDate, academicInfo.endDate)
+    }))
   });
 });
+
+const getAcademicStatus = (startDate: Date, endDate: Date) => {
+  const currentDate = new Date();
+  if (currentDate < startDate) {
+    return 'Upcoming';
+  } else if (currentDate > endDate) {
+    return 'Done';
+  } else {
+    return 'Ongoing';
+  }
+};
 
 const getAcademicInfo = catchAsync(async (req, res) => {
   const academicInfo = await academicInfoService.getAcademicInfoWithSemesterById(
     req.params.academicInfoId
   );
-  successResponse(res, httpStatus.OK, AppMessage.retrievedSuccessful, academicInfo);
+  successResponse(res, httpStatus.OK, AppMessage.retrievedSuccessful, {
+    ...academicInfo,
+    status: getAcademicStatus(academicInfo.startDate, academicInfo.endDate)
+  });
 });
 
 const updateAcademicInfo = catchAsync(async (req, res) => {
