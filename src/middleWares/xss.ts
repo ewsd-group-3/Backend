262bb27547d336b238402d72ea8259a720a4c67b
Yin-Lynn-Htun <yinlynnhtun@rezeve.com>
2024-03-27 01:29:37 +0700

    feat: Remove xss filter

--- src/middleWares/xss.ts
+++ src/middleWares/xss.ts
@@ -21,7 +21,7 @@ export const clean = <T>(data: T | string = ''): T => {
 
 const middleware = () => {
   return (req: Request, res: Response, next: NextFunction) => {
-    if (req.body) req.body = clean(req.body);
+    if (req.body) req.body = req.body;
     if (req.query) req.query = clean(req.query);
     if (req.params) req.params = clean(req.params);
     next();
